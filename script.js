72cb150ef5fe30ccb687ccb99940726fe51c849b
joao victor benedet machado <jvbm2177@gmail.com>
2024-09-15 20:18:57 -0300

    deplo

--- script.js
+++ script.js
@@ -299,3 +299,14 @@ function Reiniciar() {
 }
 
 ChecarPergunta();
+
+const themeSwitch = document.getElementById('theme-switch');
+const body = document.body;
+
+themeSwitch.addEventListener('change', function () {
+    if (this.checked) {
+        body.classList.add('dark-theme');
+    } else {
+        body.classList.remove('dark-theme');
+    }
+});
